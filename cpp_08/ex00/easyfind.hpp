a91671efcb01c55c443c44f4c3ecdb24144c086d
Nacho Fluxá <nacho.fluxa@gmail.com>
2026-06-25 12:27:48 +0200

    cpp08 reviewed

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -2,6 +2,7 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
 #include <iostream>
 #include <exception>
 
@@ -10,12 +11,10 @@ typename T::iterator easyfind(T& container, int value)
 {
 	typename T::iterator it;
 
-	for (it = container.begin(); it != container.end(); ++it)
-	{
-		if (*it == value)
-			return (it);
-	}
-	throw std::runtime_error("Value not found.");
+	it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::runtime_error("Value not found");
+	return (it);
 }
 
 
